4a5f481317bca750302b939d0cadfa3348ce3cdd
neigood <neifergonzalez@gmail.com>
2021-09-11 14:55:29 -0300

    cambio de color
    cambio

--- js/swet.js
+++ js/swet.js
@@ -1,3 +1,17 @@
 swal.fire({
     title: "Bienvenidos a mi sitio"
 });
+
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+	document.getElementById("hexadecimal").innerHTML = color;
+	document.getElementById("text").innerHTML = "Copiar Color";
+}
